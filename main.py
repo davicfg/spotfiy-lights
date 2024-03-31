299954551a9564df26e399c285f2491b43ff9f5d
davicfg <davicfg@gmail.com>
2024-03-31 19:38:29 -0300

    [WIP] Testando envs no python

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from typing import Union
 
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-
 from fastapi.templating import Jinja2Templates
+
+
+from dotenv import load_dotenv
+
 
 app = FastAPI()
 
@@ -13,6 +16,11 @@ templates = Jinja2Templates(directory="templates")
 async def read_root(request: Request):
     return templates.TemplateResponse(request=request, name="login.html")
 
+@app.get("/callback")
+def callback(request: Request):
+    return {"call": "back"}
+
+
 @app.get("/login")
 def login(request: Request):
-    return {"hello": 1}
+    return {"lo": "gin"}
